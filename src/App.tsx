f96fedcf82ca26b9d00eca2e7a318b1fa608a0bf
Jose Perez <joseperu2503@gmail.com>
2025-10-26 12:28:46 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import PromptPage from "./pages/PromptPage";
+import QuizPage from "./pages/QuizPage";
+import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
-    <>
-      <PromptPage></PromptPage>
-    </>
+    <Routes>
+      <Route path="/" element={<PromptPage />} />
+      <Route path="/quiz" element={<QuizPage />} />
+      <Route path="/result" element={<ResultPage />} />
+
+      {/* Redirección por defecto */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
+import App from "./App.tsx";
 import "./index.css";
-import PromptPage from "./pages/PromptPage.tsx";
-import QuizPage from "./pages/QuizPage.tsx";
-import ResultPage from "./pages/ResultPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<PromptPage />} />
-      <Route path="/quiz" element={<QuizPage />} />
-      <Route path="/result" element={<ResultPage />} />
-
-      {/* Redirección por defecto */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
